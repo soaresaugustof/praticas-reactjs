8b42c9d3be6d6ab1b08247e6caab9adcbc0e31d0
Augusto Soares <soaresaugusto@edu.unifor.br>
2022-03-14 16:40:58 -0300

    newTransactionModal & react-modal config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
