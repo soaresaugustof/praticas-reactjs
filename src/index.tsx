058a5f99938107c14f3f9d514a715f2457be9f30
Augusto Soares <soaresaugusto@edu.unifor.br>
2022-03-29 14:35:06 -0300

    seeds

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-03-29 09:47:31'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2022-03-31 09:01:20'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
